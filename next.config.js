a79d64d2a39e8af326fb6406bfbf8aecf5ed2308
Gurpreet Singh <gursingh7@publicisgroupe.net>
2021-12-15 16:54:37 +0100

    changes in next.config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const mongodburl = process.env.MONGODB_URI;
 const baseUrl = process.env.BASE_URL;
+const API_SECRET_KEY = process.env.API_SECRET_KEY;
 
 module.exports = {
   env: {
     mongodburl,
     baseUrl,
-    API_SECRET_KEY: process.env.API_SECRET_KEY,
+    API_SECRET_KEY,
   },
   reactStrictMode: true,
 };
