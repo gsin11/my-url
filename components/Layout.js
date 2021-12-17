96246ae1440b7cde21ab6a5f310165189b9b522d
Gurpreet Singh <gursingh7@publicisgroupe.net>
2021-12-17 16:56:41 +0100

    added favicons

--- components/Layout.js
+++ components/Layout.js
@@ -16,6 +16,24 @@ export default function Layout({ children, title }) {
           name="keywords"
           content="shorten shorturl tinyurl url save share analyze"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <div className="flex justify-center place-items-center h-screen bg-slate-50 dark:bg-slate-600 flex-col px-5">
         {children}
